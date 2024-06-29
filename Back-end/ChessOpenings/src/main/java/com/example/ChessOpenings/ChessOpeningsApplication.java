ac86e1ab72592ade262c3860ad8adef671093185
yojin <roadlroal@naver.com>
2024-06-29 19:02:11 +0900

    [Feat] ChessOpenings : Connect DB

---
+++ Back-end/ChessOpenings/src/main/java/com/example/ChessOpenings/ChessOpeningsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChessOpenings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessOpeningsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessOpeningsApplication.class, args);
+	}
+
+}
